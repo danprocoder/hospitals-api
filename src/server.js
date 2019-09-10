4bb9a0a4442675ff37b076e1dd122e3475325a8c
Daniel Austin <danielaustin718@gmail.com>
2019-09-10 12:10:48 +0100

    Husky test

--- src/server.js
+++ src/server.js
@@ -1,8 +1,8 @@
 import express from 'express'
 
-const app = express();
+const app = express()
 
-const port = process.env.PORT || 4032;
+const port = process.env.PORT || 4032
 app.listen(port, () => {
-  console.log(`Server started on port ${port}`);
-});
+  console.log(`Server started on port ${port}`)
+})
