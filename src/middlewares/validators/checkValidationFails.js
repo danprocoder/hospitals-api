36e58ce55e0ac7bd764ed7b03f4110bac81af1fa
Daniel Austin <danielaustin718@gmail.com>
2019-09-14 14:26:34 +0100

    API to add a hospital

--- src/middlewares/validators/checkValidationFails.js
+++ src/middlewares/validators/checkValidationFails.js
@@ -1,11 +1,11 @@
 import { validationResult } from 'express-validator'
 import response from '../../utils/response'
 
-export default (req, res, next) => {
+export default (message) => (req, res, next) => {
   const errors = validationResult(req).formatWith(({ msg }) => msg)
   if (!errors.isEmpty()) {
     response(res).badRequest({
-      message: 'Unable to create user account',
+      message,
       data: {
         errors: errors.mapped()
       }
--- src/routes/hospitals.js
+++ src/routes/hospitals.js
@@ -1,9 +1,12 @@
 import { Router } from 'express'
+import createHospitalValidator from '../middlewares/validators/createHospital'
 import checkApiKey from '../middlewares/checkApiKey'
 import hospitalsController from '../controllers/hospitals'
 
 const router = Router()
 
-router.get('/', checkApiKey, hospitalsController.getAll)
+router.route('/')
+  .get(checkApiKey, hospitalsController.getAll)
+  .post(createHospitalValidator, hospitalsController.addNew)
 
 export default router
